0843f776566a7b59e0276cf2a8eca28a803d41e0
nicoarias47 <ariasn1995@gmail.com>
2022-08-12 20:05:04 -0300

    estructura basica + data

---
+++ README.md
@@ -0,0 +1,5 @@
+# QUIZ
+
+## Juego de preguntas, utilizando Programacion Orientada a Objetos
+
+### Js, OOP, Html/Css
---
+++ data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    question: "cccccccccccccccccccc ?",
+    choices: ["aaaa", "bbbbb", "cccc", "ddddd"],
+    answer: "cccc",
+  },
+  {
+    question: "bbbbbbbbbbbbbbbbb ?",
+    choices: ["aaaa", "bbbb", "cccc", "ddddd"],
+    answer: "bbbb",
+  },
+  {
+    question: "aaaaaaaaaaaaaaaaaaaaa ?",
+    choices: ["aaaa", "bbbbb", "cccc", "ddddd"],
+    answer: "aaaa",
+  },
+];
